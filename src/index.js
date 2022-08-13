7f546247c273430d13cf052eed25fba155f49f64
Megan <meganmoulos@gmail.com>
2022-08-13 13:28:15 -0700

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  let form = document.getElementById('create-task-form')
+
+  let dropDown = document.createElement('select')
+  form.appendChild(dropDown)
+  dropDown.setAttribute('id', 'select-priority')
+
+  let values = ['high', 'medium', 'low']
+
+  for (const val of values) {
+      let option = document.createElement('option')
+      option.value = val
+      option.text = val
+      dropDown.appendChild(option)
+    }
+ 
+  form.addEventListener('submit', e => { 
+    e.preventDefault()
+    buildToDo(e.target["new-task-description"].value, e.target["select-priority"].value)
+    form.reset() 
+  })
 });
+
+function buildToDo(todo, priority) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  p.textContent = `${todo} Priority: ${priority} `
+  document.querySelector('#list').appendChild(p)
+  btn.textContent = 'x'
+  p.appendChild(btn)
+
+  function handleDelete(e) {
+    e.target.parentNode.remove()
+  }
+}
+
+
